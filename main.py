364df082e93567b55364f47b837bb108381fdab0
Stefaniya <>
2021-05-31 11:14:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,126 @@
+'''
+For in Loop
+'''
+
+# students = ['Bob', 'Adriana', 'Felix']
+ 
+# for stu in students: 
+#  print('hi', stu)
+
+# ======================
+
+# print(range(10))
+# print(list(range(10)))
+# print(list(range(3, 6)))
+# print(list(range(3, 12, 2)))
+# print(list(range(3, 15, 20)))
+
+# ======================
+
+# for x in range(6):
+#   print(x + 1)
+
+# for x in range(6):
+#   print(x * 2)
+
+
+# for i in range(1, 6):
+#   for j in range(i):
+#     print('*', end=" ")
+#   print()
+
+  # ======================
+
+# for i in range(1, 2):
+#     for j in range(i):
+#       print(i, end=" ")
+# #     print()  
+
+# for i in range(1, 2):
+#     for j in range(i):
+#       print(j, end=" ")
+#     print()  
+  
+
+# for i in range(3, 9):
+#     for j in range(i):
+#       print(i, end=" ")
+#     print()  
+
+
+
+# for i in range(3, 9):
+#     for j in range(i):
+#       print(j, end=" ")
+#     print()  
+
+
+# for i in range(3, 9):
+#     for j in range(i):
+#       print(i, j, end=" ")
+#     print() 
+
+
+# for i in range(3, 9):
+#     for j in range(i):
+#       print(j, end=" ")
+#     print() 
+
+# for i in range(3, 9):
+#     for j in range(i):
+#       print("*", end=" ")
+#     print() 
+
+
+# for i in range(2, 4):
+#     for j in range(i):
+#       print(i, j, end=" ")
+#     print() 
+
+
+# for i in range(5):
+#     for j in range(i):
+#       print(i, j, end=" ")
+#     print() 
+
+# for i in range(5, 2):
+#     for j in range(i):
+#       print(i, j, end=" ")
+#     print()
+
+
+'''
+While Loop
+'''
+
+# i = 1
+# while i < 10:
+#   print(i)
+#   i += 1
+
+#  ================
+#  break statement
+
+# i = 1
+# while i < 10:
+#   print(i)
+#   if i == 5:
+#      break
+#   i += 1
+
+#  =================
+#  continue statements
+
+# i = 1
+# while i < 10:
+#   i += 1
+#   if i == 3:
+#      continue
+#   print(i)
+
+#  =================
+
+# for i in range (1, 10):
+#   if i == 3:
+#       break
+#   print(i)    
